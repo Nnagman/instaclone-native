b66dd9d64bbd55093039b2d8bc6098c6a74718f0
nnagm <nnagman@gmail.com>
2021-06-29 17:51:22 +0900

    14.3 Dark Mode

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import AppLoading from "expo-app-loading";
 import React, {useState} from "react";
 import {Ionicons} from "@expo/vector-icons";
 import * as Font from "expo-font";
+import {Asset} from "expo-asset";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import {NavigationContainer} from "@react-navigation/native";
 
@@ -11,7 +12,12 @@ export default function App() {
     const preload = () => {
         const fontsToLoad = [Ionicons.font];
         const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
-        return Promise.all(fontPromises);
+        const imagesToLoad = [
+            require("./assets/logo.png"),
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
+        ];
+        const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+        return Promise.all([...fontPromises, ...imagePromises]);
     };
     if (loading) {
         return (
@@ -22,6 +28,7 @@ export default function App() {
             />
         );
     }
+
     return (
         <NavigationContainer>
             <LoggedOutNav/>
